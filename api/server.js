d6635ab8745a024e1c1210d894d3b59490f1377c
Vinícius Fachini <vinniebrasil@gmail.com>
2023-11-15 19:08:02 -0300

    fix: changed the port to avoid conflicts with the frontend

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 
